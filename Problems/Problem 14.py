f2b0f06f9335ecdf8564c2d94d2244807688adca
AbasovAmin <amin.abasov.5501@gmail.com>
2026-05-12 17:38:24 +0300

    Update Problem 14.py

--- Problems/Problem 14.py
+++ Problems/Problem 14.py
@@ -24,34 +24,3 @@ for i in range(1, 1_000_000):
         max_start = i
 
 print(max_start)
-
-
-
-#My solution for this problem which took it 19 seconds
-'''dictionary_of_numbers_and_their_chain = {}
-biggest_chain_length = 0
-biggest_chain_length_generator = 0
-
-for i in range(1,1000000):
-    starting_Number = i
-    chain_length = 1
-    
-    while starting_Number != 1:
-        
-        if starting_Number % 2 == 0:
-            starting_Number //= 2
-            chain_length += 1
-        else:
-            starting_Number *= 3
-            starting_Number += 1
-            chain_length += 1
-
-    dictionary_of_numbers_and_their_chain[i] = chain_length
-    if dictionary_of_numbers_and_their_chain[i] > biggest_chain_length:
-        biggest_chain_length = dictionary_of_numbers_and_their_chain[i]
-        biggest_chain_length_generator = i
-        
-print(biggest_chain_length_generator)'''
-
-
-
